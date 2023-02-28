95ea9d29b2227cc4e246886c3575b5494b0edbd3
Emre Guven <eguven76@gmail.com>
2023-02-28 23:30:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+/* SENARYO-1 */
+//console.log("SENARYO-1");
+let n = 11;
+const middle = Math.ceil(n / 2);
+
+for (let i = 1; i <= n; i++) {
+  let row = "";
+
+  for (let j = 1; j <= n - i; j++) {
+    row += " ";
+  }
+
+  for (let k = 0; k < 2 * i - 1; k++) {
+    row += "*";
+  }
+  if (i === middle) {
+    const middleStringIndex = Math.round(row.length / 2 + 1);
+    row =
+      row.substring(0, middleStringIndex) +
+      "REA" +
+      row.substring(middleStringIndex + 3);
+  }
+  console.log(row);
+}
+
+/* SENARYO-2 */
+
+console.log("SENARYO-2");
+
+let x = parseInt(prompt("Bir sayı giriniz:"));
+let row = "";
+
+for (let i = 0; i <= x; i++) {
+  for (let j = 0; j < x - i; j++) {
+    row += " ";
+  }
+
+  for (let j = 0; j <= i; j++) {
+    row += "*";
+  }
+  if ((i + 1) % 3 === 0) {
+    row = row.slice(0, -3);
+    row += "REA";
+  }
+  row += "\n";
+}
+console.log(row);
